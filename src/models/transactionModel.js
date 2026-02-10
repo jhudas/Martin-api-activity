1732cf55efb15ffa7ed0339a5ffd3c52869e9850
jhude <martin.jhudemichael@marsu.edu.ph>
2026-02-10 18:31:47 +0800

    csaccascsc

--- src/models/transactionModel.js
+++ src/models/transactionModel.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
     description: {
         type: String,
-        required: true
+        required: true,
+        minLength: [3, 'Description must be at least 3 characters long']
     },
     amount: {
         type: Number,
-        required: true
+        required: true,
+        min: [0.01, 'Amount must be greater than zero']
     },
     date: {
         type: Date,
